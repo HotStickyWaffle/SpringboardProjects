270797faed3d227757501ab7256976595d455077
Nick Salvemini <salvemini.nick@gmail.com>
2022-11-20 09:53:04 -0500

    completed basic functionality (click to place colors and update array, check for win and alert). polish and additional features needed

--- connect-four/connect4.js
+++ connect-four/connect4.js
@@ -5,83 +5,117 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 (red) or 2 (blue)
+let playerColor = 'red'
 
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
+const board = [
+
+]
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for (let i = 0; i < HEIGHT; i++) {
+    const arr = [];
+    for (let j = 0; j < WIDTH; j++) {
+      arr.push(null);
+    }
+    board.push(arr)
+  }
 }
 
-/** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.querySelector('#board')
 
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
+  // Build and append top row for selecting colomn to drop piece
+  const top = document.createElement('tr');
+  top.setAttribute('id', 'column-top');
+  top.addEventListener('click', handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
-    headCell.setAttribute("id", x);
+  for (let i = 0; i < WIDTH; i++) {
+    let headCell = document.createElement('td');
+    headCell.setAttribute('id', i);
     top.append(headCell);
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
-    const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
-      const cell = document.createElement("td");
-      cell.setAttribute("id", `${y}-${x}`);
+  // Build board based on specified height and width
+  for (let i = 0; i < HEIGHT; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < WIDTH; j++) {
+      const cell = document.createElement('td');
+      cell.setAttribute('id', `${i}-${j}`);
+      cell.setAttribute('class', 'empty')
       row.append(cell);
     }
     htmlBoard.append(row);
   }
 }
 
-/** findSpotForCol: given column x, return top empty y (null if filled) */
-
-function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+function findSpotForCol(column) {
+  if (document.getElementById(`5-${column}`).classList.contains('empty')) {
+    return 5;
+  }
+  else if (document.getElementById(`4-${column}`).classList.contains('empty')) {
+    return 4
+  }
+  else if (document.getElementById(`3-${column}`).classList.contains('empty')) {
+    return 3
+  }
+  else if (document.getElementById(`2-${column}`).classList.contains('empty')) {
+    return 2
+  }
+  else if (document.getElementById(`1-${column}`).classList.contains('empty')) {
+    return 1
+  }
+  else if (document.getElementById(`0-${column}`).classList.contains('empty')) {
+    return 0
+  }
+  else { return null }
 }
 
-/** placeInTable: update DOM to place piece into HTML table of board */
-
-function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+function placeInTable(row, column) {
+  const placedCell = document.getElementById(`${row}-${column}`);
+  placedCell.classList.remove('empty');
+  const placedDiv = document.createElement('div')
+  placedCell.append(placedDiv)
+  if (currPlayer === 1) {
+    //add color chip
+    placedDiv.classList.add('filledRed');
+    //update board array
+    board[row][column] = 'red'
+  } else {
+    placedDiv.classList.add('filledBlue');
+    board[row][column] = 'blue'
+  }
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  alert(msg)
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
-  // get x from ID of clicked cell
-  var x = +evt.target.id;
+  // get column from ID of clicked cell
+  const column = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
-  if (y === null) {
+  const row = findSpotForCol(column);
+  if (row === null) {
     return;
   }
 
+  //*************************************************************************************
+  console.log(row, column)
+  // **************************************************************************************
+
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
-  placeInTable(y, x);
+  placeInTable(row, column);
 
   // check for win
   if (checkForWin()) {
@@ -92,35 +126,44 @@ function handleClick(evt) {
   // TODO: check if all cells in board are filled; if so call, call endGame
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  if (currPlayer === 1) {
+    currPlayer = 2;
+    playerColor = 'blue'
+  } else {
+    currPlayer = 1;
+    playerColor = 'red'
+  }
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
 function checkForWin() {
+
   function _win(cells) {
     // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
+    //  - cells: list of four (row, column) cells
     //  - returns true if all are legal coordinates & all match currPlayer
 
+    //*************************************************************************************
+    console.log(playerColor)
+    //*************************************************************************************
+
     return cells.every(
-      ([y, x]) =>
-        y >= 0 &&
-        y < HEIGHT &&
-        x >= 0 &&
-        x < WIDTH &&
-        board[y][x] === currPlayer
+      ([row, column]) =>
+        row >= 0 &&
+        row < HEIGHT &&
+        column >= 0 &&
+        column < WIDTH &&
+        board[row][column] === playerColor
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
-
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let i = 0; i < HEIGHT; i++) {
+    for (let j = 0; j < WIDTH; j++) {
+      const horiz = [[i, j], [i, j + 1], [i, j + 2], [i, j + 3]];
+      const vert = [[i, j], [i + 1, j], [i + 2, j], [i + 3, j]];
+      const diagDR = [[i, j], [i + 1, j + 1], [i + 2, j + 2], [i + 3, j + 3]];
+      const diagDL = [[i, j], [i + 1, j - 1], [i + 2, j - 2], [i + 3, j - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
